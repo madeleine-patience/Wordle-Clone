fe2ee294ca942eb7c76a17b1d241db8949900cfb
madeleinepbelanger <madeleinepbelanger@gmail.com>
2024-11-02 08:15:18 -0700

    Wordle game complete!

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,51 +1,126 @@
 import React, { useState } from "react";
 
-import { Button } from "@mui/material";
+import { Box, Button, Typography } from "@mui/material";
+import { sample } from "new-component/src/utils";
+import { WORDS } from "../../data";
 
-// Pick a random word on every pageload.
-// const answer = sample(WORDS);
-// // To make debugging easier, we'll log the solution in the console.
-// console.info({ answer });
+const answer = sample(WORDS);
 
+let guessesWithPairs = [];
 const Game = () => {
   const [guess, setGuess] = useState("");
   const [wordleGuess, setWordleGuess] = useState([]);
-  console.log(wordleGuess);
 
-  // This is our function that will actually do the adding of the form. So we take our array of worldGuesses, and we add in the extra guess.
+  const checkGuess = () => {
+    const currentAnswer = answer.toUpperCase().split("");
+    const currentGuess = guess.toUpperCase().split("");
+    let newGuessArray = [];
+    for (let i = 0; i < 5; i++) {
+      if (currentAnswer[i] === currentGuess[i]) {
+        newGuessArray.push({
+          letter: currentGuess[i],
+          isCorrect: true,
+        });
+      } else {
+        newGuessArray.push({
+          letter: currentGuess[i],
+          isCorrect: false,
+        });
+      }
+    }
+    guessesWithPairs.push([newGuessArray]);
+  };
+
   const handleAddGuess = (guess) => {
+    const doesGuessExist = wordleGuess.some(
+      (existingGuess) => existingGuess.guess === guess
+    );
+
+    if (doesGuessExist) {
+      return;
+    }
+
     const newGuess = { guess, id: Math.random() };
     const nextGuess = [...wordleGuess, newGuess];
     setWordleGuess(nextGuess);
+
+    checkGuess();
   };
 
   return (
-    <>
-      <label htmlFor="wordle-input"> Your Guess: </label>
-      {/* this is the form that will submit everything. Here inside goes our function from above and it takes in a single guess from the value and adds it in with the function.
-      we are also reseting the value as well, and the value is guess so that has what we want to be displabed in the inptu. */}
-      <form
-        onSubmit={(event) => {
-          event.preventDefault();
-          handleAddGuess(guess);
-          setGuess("");
+    <Box
+      sx={{
+        p: 5,
+        display: "flex",
+        alignItems: "center",
+        backgroundColor: "#fe93de",
+        height: "100vh",
+        flexDirection: "column",
+      }}
+    >
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          gap: 2,
+          p: 2,
         }}
       >
-        {/* This is for a form, we've set a max length of 6, our type of text. other tyhpes include radio buttosn exct. value is what is displayed in the input. so if we tyhpe 12345. The input will show 123456. But it will have a max of 6. */}
-        {/* The on change here, is held in state. Event.target.value is the active keystroke. So i type 1, that event is 1. So when we store a input inside of the value, we hodl on to what our input says. That is how we submit our guess. */}
-        <input
-          maxLength={6}
-          type="text"
-          value={guess}
-          id="wordle-input"
-          onChange={(event) => {
-            setGuess(event.target.value);
+        <label htmlFor="wordle-input">
+          <Typography variant="h5"> Go for it</Typography>
+        </label>
+        <form
+          onSubmit={(event) => {
+            event.preventDefault();
+            handleAddGuess(guess);
+            setGuess("");
           }}
-        />
-        {/* Unfuotunately this was  abug for me from material ui. My fautl for getting too fancy! */}
-        <Button type="submit">Submit Guess!</Button>
-      </form>
-    </>
+        >
+          <input
+            minLength={5}
+            maxLength={5}
+            type="text"
+            value={guess}
+            disabled={wordleGuess.length > 5}
+            id="wordle-input"
+            onChange={(event) => {
+              setGuess(event.target.value);
+            }}
+          />
+          <Button type="submit">Submit Guess!</Button>
+        </form>
+      </Box>
+      {guessesWithPairs.map((item, index) => {
+        return (
+          <Box key={index} sx={{ display: "flex", justifyContent: "center" }}>
+            {item.map((arrays, innerIndex) => {
+              return (
+                <Box key={innerIndex} sx={{ display: "flex" }}>
+                  {arrays.map(({ isCorrect, letter }, letterIndex) => {
+                    return (
+                      <Box
+                        key={letterIndex}
+                        width={100}
+                        height={100}
+                        backgroundColor={isCorrect ? "lightGreen" : "pink"}
+                        sx={{
+                          display: "flex",
+                          justifyContent: "center",
+                          alignItems: "center",
+                        }}
+                      >
+                        <Typography variant="h5">{letter}</Typography>
+                      </Box>
+                    );
+                  })}
+                </Box>
+              );
+            })}
+          </Box>
+        );
+      })}
+    </Box>
   );
 };
 
